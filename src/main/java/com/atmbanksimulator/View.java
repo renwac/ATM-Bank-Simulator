a29d42ed39ad67a823dac9e53c1c005289ba7806
renwac <alfierenwick2006@gmail.com>
2026-04-27 17:19:15 +0100

    fixed button name lengths

--- src/main/java/com/atmbanksimulator/View.java
+++ src/main/java/com/atmbanksimulator/View.java
@@ -98,8 +98,8 @@ class View {
                 {"7",    "8",  "9",  "",  "Dep",   ""},
                 {"4",    "5",  "6",  "",  "W/D",   "Trf"},
                 {"1",    "2",  "3",  "",  "Bal",   "Fin"},
-                {"CLR",  "0",  "",   "",  "ChgPw", "Ent"},
-                {"",     "",   "",   "",  "NewAcc", ""}  };
+                {"CLR",  "0",  "",   "",  "CPw", "Ent"},
+                {"",     "",   "",   "",  "NAc", ""}  };
 
         // Build the button panel, loop through the array,
         // - For non-empty strings, create a Button
